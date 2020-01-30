3a63caba2fd2fc4e76c87148ccf98afbe8f2bb35
boxizeng <boxizeng@tencent.com>
2020-01-31 00:10:02 +0800

    feat: 1.92

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "test" ? "/" : 'https://cdn.jsdelivr.net/gh/boxizen/wedding@1.91/',
+  publicPath: process.env.NODE_ENV === "test" ? "/" : 'https://cdn.jsdelivr.net/gh/boxizen/wedding@1.92/',
 }
