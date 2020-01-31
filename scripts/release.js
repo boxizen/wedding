4703fcd63d4f13d3e58487cb91c7942c01d7cb01
boxizeng <boxizeng@tencent.com>
2020-01-31 15:20:06 +0800

    feat: log

--- scripts/release.js
+++ scripts/release.js
@@ -4,7 +4,6 @@ const conf = require('../conf');
 const cwd = "/home/travis/build/boxizen/wedding"; //path.resolve(__dirname, "../");
 const api = `https://api.github.com/repos/boxizen/wedding/releases`;
 
-
 let body = {
   "tag_name": conf.tag,
   "target_commitish": "gh-pages",
@@ -17,22 +16,29 @@ let body = {
 const TOKEN = process.argv[2];
 
 async function start() {
-  await runCommand("git", ["fetch", "origin", "gh-pages"], {
-    cwd
-  });
-  let commitId = await runCommand("git", ["rev-parse", "--short", "gh-pages"], {
-    cwd
-  });
-  commitId = commitId.replace(/\n/g,'')
-  await runCommand("git", ["tag", "-a", conf.tag, commitId, "-m", `"${conf.tag}"`], {
-    cwd
-  });
-  await runCommand("git", ["push", "--tags"], {
-    cwd
-  });
-  let res = await runCommand("curl", ["-H", `Authorization: token ${TOKEN}`, "-X", "POST", "-d", JSON.stringify(body), api], {
-    cwd
-  })
+  try {
+    let fetchRes = await runCommand("git", ["fetch", "origin", "gh-pages"], {
+      cwd
+    });
+    console.log('fetchRes:', fetchRes);
+    let commitId = await runCommand("git", ["rev-parse", "--short", "gh-pages"], {
+      cwd
+    });
+    commitId = commitId.replace(/\n/g,'')
+    await runCommand("git", ["tag", "-a", conf.tag, commitId, "-m", `"${conf.tag}"`], {
+      cwd
+    });
+    await runCommand("git", ["push", "--tags"], {
+      cwd
+    });
+    let res = await runCommand("curl", ["-H", `Authorization: token ${TOKEN}`, "-X", "POST", "-d", JSON.stringify(body), api], {
+      cwd
+    })
+    console.log('curl res:', res);
+  } catch(e) {
+    console.log(e);
+    process.exit(1);
+  }  
 }
 
 start();
