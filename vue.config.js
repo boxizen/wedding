b2560b14b17addb7a55dee5f3f72ee05d155e8bd
boxizeng <boxizeng@tencent.com>
2020-02-01 11:15:48 +0800

    feat: revert jsDelivr

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const conf = require("./conf");
 module.exports = {
-  publicPath: "/" // process.env.NODE_ENV === "test" ? "/" : `https://cdn.jsdelivr.net/gh/boxizen/wedding@${conf.tag}`,
+  publicPath: process.env.NODE_ENV === "test" ? "/" : `https://cdn.jsdelivr.net/gh/boxizen/wedding@${conf.tag}`,
 }
